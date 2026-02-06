2d65c0ce199674dfa37c6d0fb371ce2065a14faf
montaniaRoja <adolfocp1972@gmail.com>
2026-02-05 18:31:54 -0600

    prueba en eclipse

--- src/main/Recv.java
+++ src/main/Recv.java
@@ -3,7 +3,6 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
-
 import Invoice.PrintInvoice;
 import connection.SqliteConnection;
 import views.MainWindow;
